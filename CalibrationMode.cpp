456c1bf525e83f6c7eb7c4ba6c528cef9fa49fe4
joe <joseph.beuckman@usbank.com>
2017-02-09 12:35:55 -0600

    trying to load calibration factor from eeprom

--- CalibrationMode.cpp
+++ CalibrationMode.cpp
@@ -1,11 +1,17 @@
+#include <EEPROM.h>
+
 #include "CalibrationMode.h"
 #include "TareModeFunction.h"
 #include "CalibrateModeFunction.h"
 
-
 CalibrationMode::CalibrationMode(HX711 _scale, rgb_lcd _lcd) : BaseMode(_scale, _lcd) {
   
-  calibration_factor = -7050;
+  EEPROM.get(0, calibration_factor);
+  
+  if (!calibration_factor) {
+    calibration_factor = -7050;
+    EEPROM.put(0, calibration_factor);
+  }
 
 
   modeFunctions = (BaseModeFunction **) malloc(3 * sizeof(BaseModeFunction*));
