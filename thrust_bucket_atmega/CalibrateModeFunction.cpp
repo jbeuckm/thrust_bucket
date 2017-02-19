4064e74a9a2209aa1cbc943495d86aceadedaa5f
joe beuckman <joebeuckman@gmail.com>
2017-02-19 09:15:05 -0600

    better calibration sequence and motor holder CAM

--- thrust_bucket_atmega/CalibrateModeFunction.cpp
+++ thrust_bucket_atmega/CalibrateModeFunction.cpp
@@ -1,34 +1,40 @@
 #include "CalibrateModeFunction.h"
 
-CalibrateModeFunction::CalibrateModeFunction(HX711 *_scale, rgb_lcd *_lcd) : BaseModeFunction(_scale, _lcd) {
-  scale = _scale;
-  lcd = _lcd;
-  trapWheelRotation = false;
+CalibrateModeFunction::CalibrateModeFunction(HX711 *_scale, rgb_lcd *_lcd) :
+		BaseModeFunction(_scale, _lcd) {
+	scale = _scale;
+	lcd = _lcd;
+	trapWheelRotation = false;
 }
 
 String CalibrateModeFunction::getLabel() {
-  return F("[Calibr]");
+	return F("[Calibr]");
 }
 
 void CalibrateModeFunction::handleButtonDown() {
-  trapWheelRotation = true;
+
+	if (trapWheelRotation) {
+		trapWheelRotation = false;
+		lcd->setCursor(0, 0);
+		lcd->print(F("Calibrating..."));
+	} else {
+		trapWheelRotation = true;
+		adjustmentSize = CALIB_ADJUST_BALLPARK;
+	}
 }
 
 void CalibrateModeFunction::handleButtonUp() {
-  trapWheelRotation = false;
-
-  lcd->setCursor(0, 0);
-  lcd->print(F("Calibrating..."));
+	adjustmentSize = CALIB_ADJUST_FINE;
 }
 
 void CalibrateModeFunction::handleWheelRotation(int wheelRotation) {
 
-	int newScale = scale->get_scale() + 10 * wheelRotation;
+	int newScale = scale->get_scale() + adjustmentSize * wheelRotation;
 
 	scale->set_scale(newScale);
 
 	lcd->setCursor(0, 0);
 	lcd->print(newScale);
+	lcd->print("  ");
 }
-
 
--- thrust_bucket_atmega/CalibrateModeFunction.h
+++ thrust_bucket_atmega/CalibrateModeFunction.h
@@ -1,6 +1,13 @@
 #include "BaseModeFunction.h"
 
+#define CALIB_ADJUST_BALLPARK 100
+#define CALIB_ADJUST_FINE 10
+
 class CalibrateModeFunction : public BaseModeFunction {
+
+private:
+	int adjustmentSize = CALIB_ADJUST_FINE;
+
 public:
   CalibrateModeFunction(HX711 *_scale, rgb_lcd *_lcd);
   String getLabel();
